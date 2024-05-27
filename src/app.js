728d8af7a0faa8727d0d0ea1b83e0fc08da164e7
Alejandro Núñez <145801334+nunezweb@users.noreply.github.com>
2024-05-27 21:11:27 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// window.onload = () => {
+//   document.querySelector(".numCenter").innerHTML = cardNumberGenerator();
+//   const cardSymbol = cardSymbolGenerator();
+//   const cardSymbolCorners = document.querySelectorAll(".corner");
+//   cardSymbolCorners[0].innerHTML = cardSymbol;
+//   cardSymbolCorners[1].innerHTML = cardSymbol;
+//
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const element = document.getElementById("clickToShuffle");
+
+element.addEventListener("click", function() {
+  document.querySelector(".numCenter").innerHTML = cardNumberGenerator();
+  const cardSymbol = cardSymbolGenerator();
+  const cardSymbolCorners = document.querySelectorAll(".corner");
+  cardSymbolCorners[0].innerHTML = cardSymbol;
+  cardSymbolCorners[1].innerHTML = cardSymbol;
+
+  if (cardSymbol === "♦" || cardSymbol === "♥") {
+    cardSymbolCorners[0].style.color = "red";
+    cardSymbolCorners[1].style.color = "red";
+  } else {
+    cardSymbolCorners[0].style.color = "black";
+    cardSymbolCorners[1].style.color = "black";
+  }
+});
+let cardNumber = ["1", "3", "J", "8"];
+let suits = ["♦", "♥", "♠", "♣"];
+
+let cardNumberGenerator = () => {
+  let cardNumberIndex = Math.floor(Math.random() * cardNumber.length);
+
+  return cardNumber[cardNumberIndex];
+};
+
+let cardSymbolGenerator = () => {
+  let suitsIndex = Math.floor(Math.random() * suits.length);
+
+  return suits[suitsIndex];
 };
